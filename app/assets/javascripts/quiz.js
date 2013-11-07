fc4dd8005f36a06a3e9d2f6f746ffc5faefcd635
Dev Bootcamp <Apprentice@dbc34.local>
2013-11-06 16:33:56 -0800

    All quiz functionality added

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,34 +1,33 @@
 var currentQuiz = null;
+var currentQuestion = null;
 var userKey = (new Date()).toString()
-var moreQuestions = false;
+var moreQuestions = true;
 
-$.ajax({
+var startProgram = function(){
+  $.ajax({
   type: "GET",
   url: "quizzes.json"
-})
+  })
   .done(function(data){
     $.each(data.quizzes, function(index, quiz){
       var newQuiz = $("#quiz").clone()
-      newQuiz.show()
       newQuiz.html(quiz.name)
+      $(".container").append(newQuiz)
       newQuiz.on("click", function(e){
         $("#quiz").hide()
         currentQuiz = quiz.quiz_id
         beginQuiz();
       })
-      $(".container").append(newQuiz)
     })
-
   });
+}
 
 var beginQuiz = function(){
-  while (moreQuestions === false)
-  {
+  // while (moreQuestions === true)
+  // {
     next()
-  }
+  // }
 };
-
-
 
 var next = function(){
   $.ajax({
@@ -37,7 +36,45 @@ var next = function(){
     data: { session_key: userKey }
   })
   .done(function(data){
-    debugger
-    console.log(data);
+    currentQuestion = data.question.question_id
+    var newQuestion = $(".question:first").clone()
+    newQuestion.html(data.question.question)
+    newQuestion.show()
+    $(".container").append(newQuestion)
+    $.each(data.question.choices, function(index, choice){
+      var newChoice = $("#choice").clone();
+      newChoice.html(choice.choice)
+      newQuestion.append(newChoice)
+      newChoice.on("click", function(e){
+        $(".question").hide()
+        answer(choice.choice_id)
+      })
+    })
   })
 };
+
+var answer = function(choiceId){
+  $.ajax({
+    type: "POST",
+    url: "questions/" + currentQuestion + "/answers.json",
+    data: {session_key: userKey, choice_id: choiceId }
+  })
+  .done(function(data){
+    if(data.status.more_questions === false){
+      moreQuestions = false;
+      $(".question").hide()
+      showResults(data.status);
+    }
+    else{
+      next();
+    }
+  })
+}
+
+var showResults = function(data_results){
+  $(".num_correct").text(data_results.num_correct)
+  $(".num_incorrect").text(data_results.num_incorrect)
+  $("#results").show()
+}
+
+  startProgram()
