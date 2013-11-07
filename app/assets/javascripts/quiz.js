cd0b41765f6c9d5320500febd7a7b66f45043d55
Dev Bootcamp <Apprentice@dbc34.local>
2013-11-06 22:54:03 -0800

    Quiz name showing

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,73 +1,72 @@
-var currentQuiz, currentQuestion;
-var userKey = (new Date()).toString()
+// var currentQuiz, currentQuestion;
+// var userKey = (new Date()).toString()
 
-var startProgram = function(){
-  $.ajax({
-  type: "GET",
-  url: "quizzes.json"
-  })
-  .done(function(data){
-    console.log("get quizzes")
-    $.each(data.quizzes, function(index, quiz){
-      var newQuiz = $("#quiz").clone()
-      newQuiz.text(quiz.name)
-      $(".container").append(newQuiz)
-      newQuiz.on("click", function(e){
-        $(newQuiz).hide();
-        currentQuiz = quiz.quiz_id
-        next();
-      })
-    })
-  });
-}
+// var startProgram = function(){
+//   $.ajax({
+//   type: "GET",
+//   url: "quizzes.json"
+//   })
+//   .done(function(data){
+//     $.each(data.quizzes, function(index, quiz){
+//       var newQuiz = $("#quiz").clone()
+//       newQuiz.text(quiz.name)
+//       $(".container").append(newQuiz)
+//       newQuiz.on("click", function(e){
+//         $(newQuiz).hide();
+//         currentQuiz = quiz.quiz_id
+//         next();
+//       })
+//     })
+//   });
+// }
 
-var next = function(){
-  $.ajax({
-    type: "GET",
-    url: "quizzes/" + currentQuiz + "/questions/next.json",
-    data: { session_key: userKey }
-  })
-  .done(function(data){
-    currentQuestion = data.question.question_id
-    var newQuestion = $(".question:first").clone()
-    newQuestion.html(data.question.question)
-    newQuestion.show()
-    $(".container").append(newQuestion)
-    $.each(data.question.choices, function(index, choice){
-      var newChoice = $("#choice").clone();
-      newChoice.html(choice.choice)
-      newQuestion.append(newChoice)
-      newChoice.on("click", function(e){
-        $(".question").hide()
-        answer(choice.choice_id)
-      })
-    })
-  })
-};
+// var next = function(){
+//   $.ajax({
+//     type: "GET",
+//     url: "quizzes/" + currentQuiz + "/questions/next.json",
+//     data: { session_key: userKey }
+//   })
+//   .done(function(data){
+//     currentQuestion = data.question.question_id
+//     var newQuestion = $(".question:first").clone()
+//     newQuestion.html(data.question.question)
+//     newQuestion.show()
+//     $(".container").append(newQuestion)
+//     $.each(data.question.choices, function(index, choice){
+//       var newChoice = $("#choice").clone();
+//       newChoice.html(choice.choice)
+//       newQuestion.append(newChoice)
+//       newChoice.on("click", function(e){
+//         $(".question").hide()
+//         answer(choice.choice_id)
+//       })
+//     })
+//   })
+// };
 
-var answer = function(choiceId){
-  $.ajax({
-    type: "POST",
-    url: "questions/" + currentQuestion + "/answers.json",
-    data: {session_key: userKey, choice_id: choiceId }
-  })
-  .done(function(data){
-    if(data.status.more_questions === false){
-      $(".question").hide()
-      showResults(data.status);
-    }
-    else{
-      next();
-    }
-  })
-}
+// var answer = function(choiceId){
+//   $.ajax({
+//     type: "POST",
+//     url: "questions/" + currentQuestion + "/answers.json",
+//     data: {session_key: userKey, choice_id: choiceId }
+//   })
+//   .done(function(data){
+//     if(data.status.more_questions === false){
+//       $(".question").hide()
+//       showResults(data.status);
+//     }
+//     else{
+//       next();
+//     }
+//   })
+// }
 
-var showResults = function(data_results){
-  $(".num_correct").text(data_results.num_correct)
-  $(".num_incorrect").text(data_results.num_incorrect)
-  $("#results").show()
-}
-
+// var showResults = function(data_results){
+//   $(".num_correct").text(data_results.num_correct)
+//   $(".num_incorrect").text(data_results.num_incorrect)
+//   $("#results").show()
+// }
 
 
-  startProgram()
+
+//   startProgram()
--- app/assets/javascripts/sandbox.js
+++ app/assets/javascripts/sandbox.js
@@ -1,32 +1,63 @@
-// // Page loads
-// // - AJAX request to get quizzes
-// // - Display quizzes based on ajax response (json object)
+// Page loads
+// - AJAX request to get quizzes
+// - Display quizzes based on ajax response (json object)
 
-// // User clicks on quiz name
-// // - AJAX request to GET next questions
-// // - In request data send: session_key
-// // - Display first question based on ajax response (json object)
+// User clicks on quiz name
+// - AJAX request to GET next questions
+// - In request data send: session_key
+// - Display first question based on ajax response (json object)
 
-// // User clicks on an answer
-// // - AJAX request to POST answer
-// // - In request data send: session_key and choice_id
+// User clicks on an answer
+// - AJAX request to POST answer
+// - In request data send: session_key and choice_id
+
+// Create an object literal
+var Controller = {
+  // initialize with your event listeners and your quizzes
+  initialize: function(e) {
+    $('.container').on('click', '#quiz', this.getNextQuestion.bind(this))
+    $('.container').on('click', '#choice', this.postAnswer.bind(this))
+    this.getQuizzes()
+  },
+
+  getQuizzes: function(){
+    console.log("Get quizzes")
+    $.ajax({
+      type: "GET",
+      url: "quizzes.json"
+    })
+      .done (function(data){
+        $.each ( data.quizzes, function (index, quiz){
+          var $renderedQuiz = Views.quiz(quiz)
+          $(".container").append($renderedQuiz)
+        })
+      })
+  },
+
+  getNextQuestion: function(data){
+    console.log(data)
+    console.log(data.firstchild)
+  },
+
+  postAnswer: function(data){
+    console.log(data)
+  }
+}
 
 
-// var Controller = {
-//   var initialize = function(){
-//     console.log("Hello")
-//   // };
-// }
-// // SET UP EVENT LISTENERS IN INITIALIZE
+// SET UP CONTROLLERS
 
 
+// SET UP VIEWS
+var Views = {
 
-// // SET UP CONTROLLERS
+  quiz: function(quiz){
+    var newQuiz = $("#quiz").clone()
+    return newQuiz.text(quiz.name)
+  }
+}
 
 
-// // SET UP VIEWS
-
-
-// // SET UP DOCUMENT READ
-// $(document).ready(Controller.initialize.bind(Controller));
+// SET UP DOCUMENT READ
+$(document).ready(Controller.initialize.bind(Controller));
 
